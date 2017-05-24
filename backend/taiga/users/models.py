6cedc1fd09b27ae2fab27a9805d8dedbaf5325d7
Igor Kavzov <igor.kavzov@gmail.com>
2017-05-24 10:23:40 +0300

    24-05

--- backend/taiga/users/models.py
+++ backend/taiga/users/models.py
@@ -2,26 +2,21 @@ from django.db import models
 from django.contrib.auth.models import User, AbstractBaseUser
 
 
-class User(User):
+# class User(User):
+#     pass
+
+
+class User(AbstractBaseUser):
+    username = models.CharField(max_length=50)
+    email = models.EmailField(max_length=255, unique=True)
+    is_admin = models.BooleanField(null=False, blank=False, default=False)
+
+    def __str__(self):
+        return "{id}. {name}".format(id=self.id, name=self.username)
+
     class Meta:
         verbose_name = "user"
         verbose_name_plural = "users"
         ordering = ["id"]
 
     USERNAME_FIELD = "username"
-
-
-# class User(AbstractBaseUser):
-#     username = models.CharField(max_length=50)
-#     email = models.EmailField(max_length=255, unique=True)
-#     is_admin = models.BooleanField(null=False, blank=False, default=False)
-#
-#     def __str__(self):
-#         return "{id}. {name}".format(id=self.id, name=self.username)
-#
-#     class Meta:
-#         verbose_name = "user"
-#         verbose_name_plural = "users"
-#         ordering = ["id"]
-#
-#     USERNAME_FIELD = "username"
