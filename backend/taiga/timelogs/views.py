6d5c652fbf2dcf5121e1052e5cc4369c40ea2198
Igor Kavzov <igor.k@core-tech.ru>
2017-06-14 18:12:25 +0300

    Download csv file. json todo

--- backend/taiga/timelogs/views.py
+++ backend/taiga/timelogs/views.py
@@ -3,33 +3,21 @@ from django.core.serializers.json import DjangoJSONEncoder
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import permission_required
 from django.contrib import messages
+from django.http import JsonResponse
 from taiga import permissions
 from .models import Timelog
 from taiga.projects.models import Project
 from .forms import TimelogForm
 from taiga.projects.issues.models import Issue
 from taiga.users.models import User
-from taiga.projects.views import valid_id, send_err_msg, project_permission_required
-
-
-def get_paginated_timelogs(request, query_list):
-    from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-    records_on_page = 25
-
-    paginator = Paginator(query_list, records_on_page)
-    page = request.GET.get('page')
-    try:
-        timelogs = paginator.page(page)
-    except PageNotAnInteger:
-        timelogs = paginator.page(1)
-    except EmptyPage:
-        timelogs = paginator.page(paginator.num_pages)
-
-    return timelogs
+from taiga.projects.views import valid_id, send_err_msg, user_project_perms, project_permission_required
 
 
 # --- utils for view_timelogs --- #
 def get_id(param, kwargs, req):
+    """
+    Get id of project, issue etc. by param from query string or get query
+    """
     try:
         id = kwargs[param]
     except KeyError:
@@ -39,18 +27,30 @@ def get_id(param, kwargs, req):
 
 
 def get_project(project_id):
+    """
+    Get project by project id
+    """
     return Project.objects.get(pk=project_id)
 
 
 def get_project_issues(project_id):
+    """
+    Get issues of project by project id
+    """
     return Issue.objects.filter(project_id=project_id)
 
 
 def get_distinct_users(timelogs):
+    """
+    Get distinct users of the timelog list
+    """
     return User.objects.filter(pk__in=[timelog.user for timelog in timelogs.distinct('user')])
 
 
-def get_timelog(timelogs, project_id=None, issue_id=None, user_id=None):
+def get_timelogs(timelogs, project_id=None, issue_id=None, user_id=None):
+    """
+    Get timelogs of project or issue or user by its id
+    """
     if project_id:
         return timelogs.filter(issue__project__id=project_id)
     if issue_id:
@@ -65,68 +65,147 @@ def view_timelogs(request, **kwargs):
     """
     Display timelogs
     """
+    global project, issues, users, user_perms
     template = 'timelogs/timelogs_list.html'
-    params = {}
-    args = {
-        'title': 'Timelogs',
-        'timelog_form': TimelogForm,
-        'issues': Issue.objects.all(),
-        'users': User.objects.all(),
-    }
+    csvfilename = 'timelogs.csv'
+    jsonfile = 'timelogs.json'
+    title = 'Timelogs'
 
-    # initial timelogs query
+    # initial timelogs set
     timelogs = Timelog.objects.all()
 
     # project_id
-    params['project_id'] = get_id('project_id', kwargs, request)
-    if params['project_id']:
-        args['project'] = get_project(params['project_id'])
+    project_id = get_id('project_id', kwargs, request)
+    # TODO remove 'if' after remove 'Tiemlogs' menu punct (no more need cause no output of all timelogs of all projects)
+    if project_id:
+        project = get_project(project_id)
+        title += ' of &laquo;' + project.name + ' &raquo;'
         # get timelogs of the project
-        timelogs = get_timelog(timelogs, project_id=params['project_id'])
+        timelogs = get_timelogs(timelogs, project_id=project_id)
         # issues of the project only
-        args['issues'] = get_project_issues(params['project_id'])
+        issues = get_project_issues(project_id)
         # users who tracked at the project issues only
-        args['users'] = get_distinct_users(timelogs)
+        users = get_distinct_users(timelogs)
+        # authorized user perms at the project
+        user_perms = user_project_perms(request.user.id, project_id)
 
-    # issue_id
-    params['issue_id'] = get_id('issue_id', kwargs, request)
-    if params['issue_id']:
+    # filtered issue_id
+    issue_id = get_id('issue_id', kwargs, request)
+    if issue_id:
+    # TODO get project_id, if called from issue details page
         # get issue's timelogs
-        timelogs = get_timelog(timelogs, issue_id=params['issue_id'])
+        timelogs = get_timelogs(timelogs, issue_id=issue_id)
         # users who tracked at the issue only
-        args['users'] = get_distinct_users(timelogs)
+        users = get_distinct_users(timelogs)
 
-    # user_id
-    params['user_id'] = get_id('user_id', kwargs, request)
-    if params['user_id']:
+    # filtered user_id
+    user_id = get_id('user_id', kwargs, request)
+    if user_id:
         # get user's timelogs
-        timelogs = get_timelog(timelogs, user_id=params['user_id'])
+        timelogs = get_timelogs(timelogs, user_id=user_id)
         # issues which the user tracked
-        args['issues'] = args['issues'].filter(
+        issues = issues.filter(
             pk__in=[
-                timelog.issue.id for timelog in Timelog.objects.filter(user_id=params['user_id']).distinct('issue')
+                timelog.issue.id for timelog in Timelog.objects.filter(user_id=user_id).distinct('issue')
         ])
 
-    # get extra params
-    params['date_from'] = request.GET.get('date_from')
-    params['date_till'] = request.GET.get('date_till')
-    params['order'] = request.GET.get('order')
+    # extra filters
+    date_from = request.GET.get('date_from')
+    date_till = request.GET.get('date_till')
+    order = request.GET.get('order')
 
-    if params['date_from'] :
-        timelogs = timelogs.filter(date__gte=params['date_from'] )
-    if params['date_till']:
-        timelogs = timelogs.filter(date__lte=params['date_till'])
-    if params['order']:
-        timelogs = timelogs.order_by(params['order'])
+    if date_from:
+        timelogs = timelogs.filter(date__gte=date_from)
+    if date_till:
+        timelogs = timelogs.filter(date__lte=date_till)
+    if order:
+        timelogs = timelogs.order_by(order)
 
-    # get total duration of all filtered timelogs
-    duration = sum([v['duration'] for v in list(timelogs.values())])
+    if request.GET.get('format') == 'csv':
+        # some_streaming_csv_view(request, csvfilename)
+        data = export(timelogs, csvfilename, fields = ['issue', 'user', 'date', 'duration'])
+        return HttpResponse(data)
 
-    args['params'] = params
-    args['total_duration'] = duration
-    # args['timelogs'] = get_paginated_timelogs(request, timelogs.order_by('date'))
-    args['timelogs'] = timelogs
+    # total duration of filtered timelogs
+    total_duration = sum([v['duration'] for v in list(timelogs.values())])
+
+    if request.GET.get('format') == 'json':
+        # data = [v for v in timelogs.values('issue', 'user', 'date', 'duration')]
+        data = timelogs.values('issue', 'user', 'date', 'duration')
+# TODO
+        jsondata = JsonResponse(list(data), safe=False)
+        response = HttpResponse(jsondata, content_type='text/json')
+        # response['Content-Disposition'] = 'attachment; filename={}'.format(jsonfile)
+        return response
+
+    args = {
+        'title': title,
+        'timelog_form': TimelogForm,
+        'project': project,
+        'issues': issues,
+        'issue_id': issue_id,
+        'users': users,
+        'user_id': user_id,
+        'user_perms': user_perms,
+        'date_from': date_from,
+        'date_till': date_till,
+        'order': order,
+        'timelogs': timelogs,
+        'total_duration': total_duration,
+        'csvfile': csvfilename
+    }
+
     return render(request, template, args)
+
+
+import csv
+from django.http import HttpResponse, StreamingHttpResponse
+
+
+class Echo(object):
+    """An object that implements just the write method of the file-like
+    interface.
+    """
+    def write(self, value):
+        """Write the value by returning it, instead of storing in a buffer."""
+        return value
+
+
+def some_streaming_csv_view(request, csvfile):
+    rows = (["Row {}".format(idx), str(idx)] for idx in range(65536))
+    pseudo_buffer = Echo()
+    writer = csv.writer(pseudo_buffer)
+    response = StreamingHttpResponse((writer.writerow(row) for row in rows),
+                                     content_type="text/csv")
+    response['Content-Disposition'] = 'attachment; filename="{}".format(csvfile)'
+    return response
+
+
+def export(qs, filename, fields=None):
+    model = qs.model
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename={}'.format(filename)
+    writer = csv.writer(response)
+    # Write headers to CSV file
+    if fields:
+        headers = fields
+    else:
+        headers = []
+        for field in model._meta.fields:
+            headers.append(field.name)
+    writer.writerow(headers)
+    # Write data to CSV file
+    for obj in qs:
+        row = []
+        for field in headers:
+            if field in headers:
+                val = getattr(obj, field)
+                if callable(val):
+                    val = val()
+                row.append(val)
+        writer.writerow(row)
+    # Return CSV file to browser as download
+    return response
 
 
 @project_permission_required('timelogs.add_timelog', '/timelogs/')
